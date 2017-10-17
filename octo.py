bd108d2824f104c5d9cba2942dc91f6b84ddd9dc
Cooper Dalrymple <ogrecooper@ogrecooper.cleverogre.com>
2017-10-17 13:48:37 -0500

    Added midi channel argument and midi callback response.

--- octo.py
+++ octo.py
@@ -12,8 +12,14 @@ from octomidi import OctoMidi
 from octoaudio import OctoAudio
 
 def usage():
-	print 'usage: octo.py [-d <device>] [-b <buffersize>] [-l <localmedia>] [-m <storagemedia>]'
+	print 'usage: octo.py [-d <device>] [-b <buffersize>] [-l <localmedia>] [-m <storagemedia>] [-c <midichannel>]'
 	sys.exit(2)
+
+def handle_midi(note):
+	audio.stop()
+	if note < len(files.getfiles()):
+		audio.load(files.getfiles()[note])
+		audio.play()
 
 if __name__ == '__main__':
 
@@ -21,6 +27,7 @@ if __name__ == '__main__':
 	buffersize = 512
 	localmedia = './media'
 	storagemedia = False
+	midichannel = 1
 
 	opts, args = getopt.getopt(sys.argv[1:], 'dbmlh')
 	for o, a in opts:
@@ -32,22 +39,28 @@ if __name__ == '__main__':
 			storagemedia = a
 		elif o == '-l':
 			localmedia = a
+		elif o == '-c':
+			if a == "all":
+				midichannel = a
+			else:
+				midichannel = int(a)
 		elif o == '-h':
 			usage()
 
+	# Initialize and build file list (local and storage)
 	files = OctoFiles(localmedia)
-
 	if storagemedia:
 		usb = OctoUsb(storagemedia)
 		if usb.getpath():
 			files.append(usb.getfiles())
 
-	print files.getfiles()
-
+	# Initialize Audio
 	audio = OctoAudio(device, buffersize, files.getfiles()[2])
-	audio.play()
 
-	midi = OctoMidi()
+	# Initialize Midi
+	midi = OctoMidi(midichannel)
+	midi.set_callback(handle_midi)
+	midi.open()
 
 	# Wait for keyboard interrupt
 	print "Entering main loop. Press Control-C to exit."
