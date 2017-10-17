d48fb1e50cae335e5af97ea600a17365694c3e3c
Cooper Dalrymple <ogrecooper@ogrecooper.cleverogre.com>
2017-10-17 13:21:30 -0500

    Added local directory parameter, OctoFiles, OctoAudio, OctoMidi, and keyboard interrupt.

--- octo.py
+++ octo.py
@@ -3,23 +3,26 @@
 # Octopy: 8 channel midi-activated audio player on the Raspberry Pi platform
 
 import sys
-import wave
 import getopt
-import alsaaudio
+import time
 
 from octofiles import OctoFiles
+from octofiles import OctoUsb
+from octomidi import OctoMidi
+from octoaudio import OctoAudio
 
 def usage():
-	print 'usage: octo.py [-d <device>] [-b <buffersize>] [-m <storagemedia>]'
+	print 'usage: octo.py [-d <device>] [-b <buffersize>] [-l <localmedia>] [-m <storagemedia>]'
 	sys.exit(2)
 
 if __name__ == '__main__':
 
 	device = 'default'
 	buffersize = 512
-	storagemedia = 'sda1'
+	localmedia = './media'
+	storagemedia = False
 
-	opts, args = getopt.getopt(sys.argv[1:], 'dbm:h:')
+	opts, args = getopt.getopt(sys.argv[1:], 'dbmlh')
 	for o, a in opts:
 		if o == '-d':
 			device = a
@@ -27,8 +30,33 @@ if __name__ == '__main__':
 			buffersize = int(a)
 		elif o == '-m':
 			storagemedia = a
+		elif o == '-l':
+			localmedia = a
 		elif o == '-h':
 			usage()
 
+	files = OctoFiles(localmedia)
 
-	files = OctoFiles(storagemedia)
+	if storagemedia:
+		usb = OctoUsb(storagemedia)
+		if usb.getpath():
+			files.append(usb.getfiles())
+
+	print files.getfiles()
+
+	audio = OctoAudio(device, buffersize, files.getfiles()[2])
+	audio.play()
+
+	midi = OctoMidi()
+
+	# Wait for keyboard interrupt
+	print "Entering main loop. Press Control-C to exit."
+	try:
+		while True:
+			time.sleep(1)
+	except KeyboardInterrupt:
+		print ""
+	finally:
+		print "Exit."
+		audio.stop()
+		midi.close()
