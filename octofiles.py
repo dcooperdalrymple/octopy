f44428b5dc6d1e4214abb9283412dd6a7b83e159
Cooper Dalrymple <ogrecooper@ogrecooper.cleverogre.com>
2017-10-17 13:19:50 -0500

    Added local audio file directory listing.

--- octofiles.py
+++ octofiles.py
@@ -2,11 +2,35 @@ import os
 import subprocess
 
 class OctoFiles:
+	def __init__(self, dirpath):
+		self.path = dirpath
+		self.files = []
+		self.__locate_files()
+
+	def __locate_files(self):
+		for (dirpath, dirnames, filenames) in os.walk(self.path):
+			for x in filenames:
+				if x.endswith(".wav"):
+					self.files.append(os.path.join(dirpath, x))
+			break
+
+	def append(self, add_files):
+		if add_files:
+			self.extend(add_files)
+
+	def getfiles(self):
+		if not self.files:
+			return False
+		else:
+			return self.files
+
+class OctoUsb:
 	def __init__(self, storagemedia):
 		self.media = storagemedia
 		self.mount = "~/thumb"
 		self.files = []
-		if self.__check_media():
+		self.found = self.__check_media()
+		if self.found:
 			self.__mount_media()
 			self.__list_files()
 
@@ -14,7 +38,6 @@ class OctoFiles:
 		output = subprocess.Popen("lsblk -o name -n -s -l", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 		for out in output.stdout:
-			print out
 			if self.media in out:
 				return True
 
@@ -23,7 +46,7 @@ class OctoFiles:
 	def __mount_media(self):
 		if not os.path.exists(self.mount):
 			os.makedirs(self.mount)
-		else:
+		elif os.path.ismount(self.mount):
 			subprocess.call("umount " + self.mount, shell=True)
 		subprocess.call("mount /dev/" + self.media + " " + self.mount, shell=True)
 		return True
@@ -31,9 +54,19 @@ class OctoFiles:
 	def __list_files(self):
 		for (dirpath, dirnames, filenames) in os.walk(self.mount):
 			for x in filenames:
-				print x
 				if x.endswith(".wav"):
-					self.files.append(os.path.join(dirpath, x));
-			break;
-		print self.files
+					self.files.append(os.path.join(dirpath, x))
+			break
 		return True
+
+	def getpath():
+		if self.found:
+			return self.mount
+		else:
+			return False
+
+	def getfiles():
+		if not self.files:
+			return False
+		else:
+			return self.files
