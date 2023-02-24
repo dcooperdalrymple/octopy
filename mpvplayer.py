788ef06a106c2943eb7e3832c29960b38e7c4efd
dcooperdalrymple <dcooperdalrymple@gmail.com>
2023-02-24 13:12:30 -0600

    Fixed issues with MPV player arguments.

--- mpvplayer.py
+++ mpvplayer.py
@@ -8,16 +8,17 @@ class MPVPlayer(VideoPlayerProcess):
 
     def get_arguments(self):
         args = [self.get_command()]
+        args.append('--no-config') # Prevent loading local config
         args.append('--fs') # Fullscreen
         args.append('--fs-screen=all') # Disable on all screens
         args.append('--no-audio') # Remove audio
         args.append('--no-terminal') # Disable terminal output
         #args.append('--ontop') # Force on top
+        args.append('--no-input-default-bindings') # Disable keyboard bindings
         args.append('--input-vo-keyboard=no')
-        args.append('--no-input-default-bindings')
-        args.append('--no-config')
-        args.append('--no-osd-bar')
-        args.append('--no-osc')
+        args.append('--no-osc') # Disable On-Screen Controller
+        args.append('--no-osd-bar') # Disable On-Screen Display
+        args.append('--loop')
         args.append(self.path)
         return args
 
